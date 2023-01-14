24386c0c11836f836d82397a0f9b66c262d6c71f
Ali <gholamiali41380@gmail.com>
2023-01-14 14:01:00 +0330

    new file

---
+++ 1.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main (){
+int n;
+int i;
+int f=1;
+scanf("%d",&n);
+
+if (n>=0 ,n<=10)
+{  
+     i=1,
+    i<=n;
+    i++;
+    f=f*i;
+    printf("%d=%d",n,f);
+}
+
+
+    return 0;
+}
